a89886b185acfe19c9f635d2442b559b3ffcf364
Ron <ron23231@gmail.com>
2020-09-25 13:59:21 +0300

    tied up the files and added textures

--- uiScript.js
+++ uiScript.js
@@ -51,24 +51,38 @@ function changeStyle(name) {
         case "def":
             if (document.getElementById("boardDiv").childNodes.length != 0)
                 document.getElementById("boardDiv").childNodes[0].style = "";
-            document.body.style.backgroundImage = "url(DefaultBG.jpg)";
+            document.body.style.backgroundImage = "url(images/DefaultBG.jpg)";
             break;
         case "sand":
             if (document.getElementById("boardDiv").childNodes.length != 0)
-                document.getElementById("boardDiv").childNodes[0].style = "--color: rgba(92, 152, 56, 0.6);background: -webkit-linear-gradient(left, rgba(233, 223, 196, 1) 0%, rgba(233, 223, 196, 1) 1%, rgba(237, 227, 200, 1) 2%, rgba(237, 227, 200, 1) 24%, rgba(235, 221, 195, 1) 25%, rgba(233, 223, 196, 1) 48%, rgba(235, 221, 195, 1) 49%, rgba(230, 216, 189, 1) 52%, rgba(230, 216, 189, 1) 53%, rgba(233, 219, 192, 1) 54%, rgba(230, 216, 189, 1) 55%, rgba(230, 216, 189, 1) 56%, rgba(233, 219, 192, 1) 57%, rgba(230, 216, 189, 1) 58%, rgba(230, 216, 189, 1) 73%, rgba(233, 219, 192, 1) 74%, rgba(233, 219, 192, 1) 98%, rgba(235, 221, 195, 1) 100%), var(--color);background-repeat: repeat, repeat;background-size: auto auto, auto auto;background-repeat: repeat;background-size: auto auto;background-size: 63px;background-blend-mode: multiply;border: 1em ridge var(--color);"
-            document.body.style.backgroundImage = "url(sand.jpg)";
+                document.getElementById("boardDiv").childNodes[0].style = generateStyle("rgba(92, 152, 56, 0.6)");
+            document.body.style.backgroundImage = "url(images/sand.jpg)";
             break;
         case "black":
             if (document.getElementById("boardDiv").childNodes.length != 0)
-                document.getElementById("boardDiv").childNodes[0].style = "--color: #5d6a78b3;background: -webkit-linear-gradient(left, rgba(233, 223, 196, 1) 0%, rgba(233, 223, 196, 1) 1%, rgba(237, 227, 200, 1) 2%, rgba(237, 227, 200, 1) 24%, rgba(235, 221, 195, 1) 25%, rgba(233, 223, 196, 1) 48%, rgba(235, 221, 195, 1) 49%, rgba(230, 216, 189, 1) 52%, rgba(230, 216, 189, 1) 53%, rgba(233, 219, 192, 1) 54%, rgba(230, 216, 189, 1) 55%, rgba(230, 216, 189, 1) 56%, rgba(233, 219, 192, 1) 57%, rgba(230, 216, 189, 1) 58%, rgba(230, 216, 189, 1) 73%, rgba(233, 219, 192, 1) 74%, rgba(233, 219, 192, 1) 98%, rgba(235, 221, 195, 1) 100%), var(--color);background-repeat: repeat, repeat;background-size: auto auto, auto auto;background-repeat: repeat;background-size: auto auto;background-size: 63px;background-blend-mode: multiply;border: 1em ridge var(--color);"
-            document.body.style.backgroundImage = "url(black.jpg)";
+                document.getElementById("boardDiv").childNodes[0].style = generateStyle("#5d6a78b3");
+            document.body.style.backgroundImage = "url(images/black.jpg)";
             break;
         case "blue":
             if (document.getElementById("boardDiv").childNodes.length != 0)
-                document.getElementById("boardDiv").childNodes[0].style = "--color: #bf607e99;background: -webkit-linear-gradient(left, rgba(233, 223, 196, 1) 0%, rgba(233, 223, 196, 1) 1%, rgba(237, 227, 200, 1) 2%, rgba(237, 227, 200, 1) 24%, rgba(235, 221, 195, 1) 25%, rgba(233, 223, 196, 1) 48%, rgba(235, 221, 195, 1) 49%, rgba(230, 216, 189, 1) 52%, rgba(230, 216, 189, 1) 53%, rgba(233, 219, 192, 1) 54%, rgba(230, 216, 189, 1) 55%, rgba(230, 216, 189, 1) 56%, rgba(233, 219, 192, 1) 57%, rgba(230, 216, 189, 1) 58%, rgba(230, 216, 189, 1) 73%, rgba(233, 219, 192, 1) 74%, rgba(233, 219, 192, 1) 98%, rgba(235, 221, 195, 1) 100%), var(--color);background-repeat: repeat, repeat;background-size: auto auto, auto auto;background-repeat: repeat;background-size: auto auto;background-size: 63px;background-blend-mode: multiply;border: 1em ridge var(--color);"
-            document.body.style.backgroundImage = "url(blue1.jpg)";
+                document.getElementById("boardDiv").childNodes[0].style = generateStyle("#bf607e99");
+            document.body.style.backgroundImage = "url(images/blue1.jpg)";
+            break;
+        case "cinder":
+            if (document.getElementById("boardDiv").childNodes.length != 0)
+                document.getElementById("boardDiv").childNodes[0].style = generateStyle("rgb(180, 50, 50)");
+            document.body.style.backgroundImage = "url(images/black.jpg)";
+            break;
+        case "egypt":
+            if (document.getElementById("boardDiv").childNodes.length != 0)
+                document.getElementById("boardDiv").childNodes[0].style = generateStyle("rgb(180, 50, 50)");
+            document.body.style.backgroundImage = "url(images/black.jpg)";
             break;
     }
+}
+
+function generateStyle(color) {
+    return "--color: " + color + ";background: -webkit-linear-gradient(left, rgba(233, 223, 196, 1) 0%, rgba(233, 223, 196, 1) 1%, rgba(237, 227, 200, 1) 2%, rgba(237, 227, 200, 1) 24%, rgba(235, 221, 195, 1) 25%, rgba(233, 223, 196, 1) 48%, rgba(235, 221, 195, 1) 49%, rgba(230, 216, 189, 1) 52%, rgba(230, 216, 189, 1) 53%, rgba(233, 219, 192, 1) 54%, rgba(230, 216, 189, 1) 55%, rgba(230, 216, 189, 1) 56%, rgba(233, 219, 192, 1) 57%, rgba(230, 216, 189, 1) 58%, rgba(230, 216, 189, 1) 73%, rgba(233, 219, 192, 1) 74%, rgba(233, 219, 192, 1) 98%, rgba(235, 221, 195, 1) 100%), var(--color);background-repeat: repeat, repeat;background-size: auto auto, auto auto;background-repeat: repeat;background-size: auto auto;background-size: 63px;background-blend-mode: multiply;border: 1em ridge var(--color);"
 }
 
 function changeHover(startAs = undefined) {
